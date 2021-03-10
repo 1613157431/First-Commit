1db6d44f8d860b1a4531bf6d0403346ece9ae315
付英军 <fuyingjun@alibaba-inc.com>
2021-03-10 17:36:42 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# GOGOCODE Starter
+
+GoGoCode 上手示例
+
+# Install
+
+```
+    npm install 
+```
+
+# Start
+
+```
+    npm run start 
+```
---
+++ src/start.js
@@ -0,0 +1,28 @@
+const $ = require('gogocode');
+// from https://gogocode.io/
+const from = `
+import a from 'a';
+console.log('get A');
+var b = console.log();
+console.log.bind();
+var c = console.log;
+console.log = func;
+`;
+
+const output = $(from)
+    .replace(`var $_$ = console.log()`, `var $_$ = void 0`)
+    .replace(`var $_$ = console.log`, `var $_$ = function(){}`)
+    .find(`console.log()`)
+    .remove()
+    .generate();
+console.log(output);
+
+const to = `
+import a from 'a';
+var b = void 0;
+console.log.bind()
+var c = function(){};
+console.log = func
+`;
+
+// output code and to code should be same
